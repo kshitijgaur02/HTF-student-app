32f04a65f4b5a9e7723b6ecf47865c1078b54a28
kshitijgaur02 <iamkshitijgaur@gmail.com>
2021-10-23 17:21:58 +0530

    home page basic

--- student-app/src/component/HomePage.js
+++ student-app/src/component/HomePage.js
@@ -1,21 +1,38 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './HomePage.css';
 import { Link } from 'react-router-dom';
 
 
 function HomePage() {
   return (
-    <div>
+    <div className='container'>
         <h1>Home</h1>
+
+      <div className='row'>
         <Link to='/FinanceTracker'>
-            <button className='btn btn-primary'>Finance Tracker</button>
+            <button className='btn btn-info btntext'>Finance Tracker</button>
         </Link>
+      </div>  
+        
+      <div className='row'>
         <Link to='/TaskTracker'>
-            <button className='btn btn-primary'>Task Tracker</button>
+            <button className='btn btn-info btntext'>Task Tracker</button>
         </Link>
+      </div>
+
+      <div className='row'>
         <Link to='/Schedule'>
-            <button className='btn btn-primary'>Schedule</button>
+            <button className='btn btn-info btntext'>Schedule</button>
         </Link>
+      </div>  
+
+      <div className='row logoutrow'>
+        <Link to='/'>
+            <button className='btn btn-dark'>Log Out</button>
+        </Link>
+      </div> 
+        
     </div>    
         
     
